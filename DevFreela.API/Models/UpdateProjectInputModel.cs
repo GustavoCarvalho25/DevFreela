2c94e3781109e8f9ae19ca1f62b6b12778073e31
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-14 21:17:17 -0300

    @GustavoCarvalho25 - Adicionando modelo de update de input de projeto

---
+++ DevFreela.API/Models/UpdateProjectInputModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DevFreela.API.Models
+{
+    public class UpdateProjectInputModel
+    {
+        public int IdProject { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal TotalCost { get; set; }
+    }
+}
