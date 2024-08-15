28e6d655cd184c0472f9c98ee114c70df7eca4fe
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-14 21:17:29 -0300

    @GustavoCarvalho25 - Adicionando Controller de Skills

---
+++ DevFreela.API/Controllers/SkillsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/skills")]
+    [ApiController]
+    public class SkillsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+    }
+}
