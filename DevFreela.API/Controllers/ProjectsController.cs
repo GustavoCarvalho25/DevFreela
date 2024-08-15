23dbed512cc30f955142ae718cf25a2efb5a4930
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-14 21:16:04 -0300

    @GustavoCarvalho25 - Adicionando Controller de Projetos

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,66 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [ApiController]
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        // GET : api/projects?search=value
+        [HttpGet]
+        public IActionResult Get(string search)
+        {
+            return Ok();
+        }
+
+        // GET : api/projects/1234
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        // POST : api/projects
+        [HttpPost]
+        public IActionResult Post(CreateProjectInputModel model)
+        {
+            return CreatedAtAction(nameof(GetById), new {id = 1}, model);
+        }
+
+        // PUT : api/projects/1234
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UpdateProjectInputModel model)
+        {
+            return NoContent();
+        }
+
+        // Delete : api/projects/1234
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+
+        // POST : api/projects/1234/start
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+        //PUT : api/projects/1234/complete
+        [HttpPut("{id}/delete")]
+        public IActionResult Complete(int id)
+        {
+            return NoContent();
+        }
+
+        //POST : api/projects/1234/comments
+        [HttpPost("{id}")]
+        public IActionResult PostComment(int id, CreateProjectCommentInputModel model)
+        {
+            return Ok();
+        }
+    }
+}
