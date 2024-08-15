0f1630295cde39e016393d8fa8c68e9e76dee2f7
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-14 21:17:43 -0300

    @GustavoCarvalho25 - Adicionando Controller de Usuarios

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+    }
+}
