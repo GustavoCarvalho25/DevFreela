70228186dad9d71a298214a636c55e9fa7e7c6a0
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-14 21:16:41 -0300

    @GustavoCarvalho25 - Adicionando modelo de Input de comentario de projeto

---
+++ DevFreela.API/Models/CreateProjectCommentInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DevFreela.API.Models
+{
+    public class CreateProjectCommentInputModel
+    {
+        public string Content { get; set; }
+        public int IdProject { get; set; }
+        public int IdUser { get; set; }
+    }
+}
