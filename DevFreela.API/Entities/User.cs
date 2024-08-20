9e4d3fbea1f5dc3979ee64a335c6f83852575212
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-20 19:45:34 -0300

    @GustavoCarvalho25 - Adicionando entidade Usuario

--- DevFreela.API/Entities/User.cs
+++ DevFreela.API/Entities/User.cs
@@ -6,6 +6,10 @@
         public string Email { get; private set; }
         public DateTime BirthDate { get; private set; }
         public bool Active { get; private set; }
+        public List<UserSkill> Skills { get; private set; }
+        public List<Project> OwnedProjects { get; private set; }
+        public List<Project> FreelanceProjects { get; private set; }
+        public List<ProjectComment> ProjectComments { get; private set; }
 
         public User(string fullName, string email, DateTime birthDate) : base()
         {
@@ -13,6 +17,11 @@
             Email = email;
             BirthDate = birthDate;
             Active = true;
+
+            Skills = [];
+            OwnedProjects = [];
+            FreelanceProjects = [];
+            ProjectComments = [];
         }
     }
 }
