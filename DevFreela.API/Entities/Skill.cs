c7026d9a3e32a00952e242ee344d70d0a490fd96
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-20 19:45:20 -0300

    @GustavoCarvalho25 - Criação de entidade Skill

---
+++ DevFreela.API/Entities/Skill.cs
@@ -0,0 +1,12 @@
+﻿namespace DevFreela.API.Entities
+{
+    public class Skill : BaseEntity
+    {
+        public string Description { get; private set; }
+
+        public Skill(string description) : base()
+        {
+            Description = description;
+        }
+    }
+}
