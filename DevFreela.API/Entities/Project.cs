884d8943d50d9411c856adaaadf235fbd05e7023
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-20 18:28:11 -0300

    @GustavoCarvalho25 - Criando entidade de Projetos e comentários de projetos

---
+++ DevFreela.API/Entities/Project.cs
@@ -0,0 +1,39 @@
+﻿using DevFreela.API.Enums;
+
+namespace DevFreela.API.Entities
+{
+    public class Project : BaseEntity
+    {
+        public Project(string title, string description, int idClient, int idFreelancer, decimal totalCost)
+        {
+            Title = title;
+            Description = description;
+            IdClient = idClient;
+            IdFreelancer = idFreelancer;
+            TotalCost = totalCost;
+
+            Status = ProjectStatusEnum.Created;
+            Comments = [];
+        }
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public int IdClient { get; private set; }
+        public User Client { get; private set; }
+        public int IdFreelancer { get; private set; }
+        public User Freelancer { get; private set; }
+        public decimal TotalCost { get; private set; }
+        public DateTime? StartedAt { get; private set; }
+        public DateTime? CompletedAt { get; private set; }
+        public ProjectStatusEnum Status { get; private set; }
+        public List<ProjectComment> Comments { get; private set; }
+
+        public void Cancel()
+        {
+            if(Status == ProjectStatusEnum.InProgress || Status == ProjectStatusEnum.Canceled)
+            {
+
+            }
+        }
+}
+}
