f5a7d68d79cda0f9f1f5fea0a89b812829e48146
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-20 18:27:54 -0300

    @GustavoCarvalho25 - Criando entidade de usuario

---
+++ DevFreela.API/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿namespace DevFreela.API.Entities
+{
+    public class User : BaseEntity
+    {
+        public string FullName { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public bool Active { get; private set; }
+
+        public User(string fullName, string email, DateTime birthDate) : base()
+        {
+            FullName = fullName;
+            Email = email;
+            BirthDate = birthDate;
+            Active = true;
+        }
+    }
+}
