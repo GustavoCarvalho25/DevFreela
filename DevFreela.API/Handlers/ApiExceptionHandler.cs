78b9412a871dcff6feddb91bfed19c215f839979
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-20 16:40:31 -0300

    @GustavoCarvalho25 - Criando Handler para exceções

---
+++ DevFreela.API/Handlers/ApiExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Handlers
+{
+    public class ApiExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            var details = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "An error occurred while processing your request.",
+            };
+
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            await httpContext.Response.WriteAsJsonAsync(details, cancellationToken);
+
+            return true;
+        }
+    }
+}
