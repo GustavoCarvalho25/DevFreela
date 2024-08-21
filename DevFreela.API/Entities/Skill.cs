ab1096a8a1dce41c057882aa2dced1a1503fd88c
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-21 13:06:38 -0300

    @GustavoCarvalho25 - Corrigindo entidade Skill

--- DevFreela.API/Entities/Skill.cs
+++ DevFreela.API/Entities/Skill.cs
@@ -3,6 +3,7 @@
     public class Skill : BaseEntity
     {
         public string Description { get; private set; }
+        public List<UserSkill> UserSkills { get; private set; }
 
         public Skill(string description) : base()
         {
