3d450b75174113495bf4653bff850d70d9b89269
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-21 13:08:32 -0300

    @GustavoCarvalho25 - Ajustando models de Skills e User Skills

---
+++ DevFreela.API/Models/CreateSkillInputModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DevFreela.API.Models
+{
+    public class CreateSkillInputModel
+    {
+        public string Description { get; set; }
+    }
+}
---
+++ DevFreela.API/Models/UserSkillsCreateInputModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DevFreela.API.Models
+{
+    public class UserSkillsCreateInputModel
+    {
+        public int[] SkillIds { get; set; }
+        public int IdUser { get; set; }
+    }
+}
