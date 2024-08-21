358bcdf464af4f9c5ee740147a92e21d52dce791
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-21 13:08:01 -0300

    @GustavoCarvalho25 - Alterando models de Visualização de projetos

---
+++ DevFreela.API/Models/ProjectItemViewModel.cs
@@ -0,0 +1,27 @@
+﻿using DevFreela.API.Entities;
+
+namespace DevFreela.API.Models
+{
+    public class ProjectItemViewModel
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string ClientName { get; private set; }
+        public string FreelancerName { get; private set; }
+        public decimal TotalCost { get; private set; }
+
+        public ProjectItemViewModel(
+            int id,
+            string title,
+            string clientName,
+            string freelancerName,
+            decimal totalCost)
+        {
+            Id = id;
+            Title = title;
+            ClientName = clientName;
+            FreelancerName = freelancerName;
+            TotalCost = totalCost;
+        }
+    }
+}
