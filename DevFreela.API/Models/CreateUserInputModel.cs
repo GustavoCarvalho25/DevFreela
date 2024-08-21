6726deeb707cbd994781b1e3b503296bf4f9cb8f
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-21 13:07:15 -0300

    @GustavoCarvalho25 - Ajustando input model de criação de usuario

---
+++ DevFreela.API/Models/CreateUserInputModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DevFreela.API.Models
+{
+    public class CreateUserInputModel
+    {
+        public string FullName { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+    }
+}
