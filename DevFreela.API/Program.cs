1379e0d12e34ee0586a73bec45fae13c2d11478e
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-23 12:52:41 -0300

    @GustavoCarvalho25 - Adicionando conexão ao SQLServer via ConnectionString

--- DevFreela.API/Program.cs
+++ DevFreela.API/Program.cs
@@ -11,7 +11,8 @@ builder.Services.Configure<FreelanceTotalCostConfig>(
     builder.Configuration.GetSection("FreelanceTotalCostConfig")
 );
 
-builder.Services.AddDbContext<DevFreelaDbContext>(o => o.UseInMemoryDatabase("DevFreela"));
+var connectionString = builder.Configuration.GetConnectionString("DevFreelaCs");
+builder.Services.AddDbContext<DevFreelaDbContext>(o => o.UseSqlServer(connectionString));
 
 builder.Services.AddScoped<IConfigServices, ConfigServices>();
 
