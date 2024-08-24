4e38df4a60e7146704649c8504a02ddc97dab4b9
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-23 23:43:59 -0300

    @GustavoCarvalho25 - Refatorando projeto e adicionando Core para arquitetura em camadas

---
+++ DevFreela.Core/Entities/Skill.cs
@@ -0,0 +1,13 @@
+﻿namespace DevFreela.Core.Entities
+{
+    public class Skill : BaseEntity
+    {
+        public string Description { get; private set; }
+        public List<UserSkill> UserSkills { get; private set; }
+
+        public Skill(string description) : base()
+        {
+            Description = description;
+        }
+    }
+}
