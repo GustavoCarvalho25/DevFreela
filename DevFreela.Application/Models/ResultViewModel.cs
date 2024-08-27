9e50fe742ae65e723ab87c4978c30b648cdea370
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-27 17:48:27 -0300

    @GustavoCarvalho25 - Implementando padrão de ResultModel

---
+++ DevFreela.Application/Models/ResultViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Models
+{
+    public class ResultViewModel
+    {
+
+        public bool IsSuccess { get; private set; }
+        public string Message { get; private set; }
+
+        public ResultViewModel(bool isSuccess = true, string message = "")
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+        public static ResultViewModel Success()
+            => new();
+
+        public static ResultViewModel Error(string message)
+            => new(false, message);
+
+    }
+    public class ResultViewModel<T> : ResultViewModel
+    {
+        public T? Data { get; private set; }
+        public ResultViewModel(T? data, bool isSuccess = true, string message = "") : base(isSuccess, message)
+        {
+            Data = data;
+        }
+
+        public static ResultViewModel<T> Success(T data)
+            => new(data);
+
+        public static ResultViewModel<T> Error(string message)
+            => new(default, false, message);
+    }
+}
