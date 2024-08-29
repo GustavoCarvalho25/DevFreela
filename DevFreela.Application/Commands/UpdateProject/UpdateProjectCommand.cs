09cf5d102d37c6ffea01a580d3d359ef8fa1cfe5
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-28 21:15:24 -0300

    @GustavoCarvalho25 - Adicionando Command Update Project e passando projeto para CQRS

---
+++ DevFreela.Application/Commands/UpdateProject/UpdateProjectCommand.cs
@@ -0,0 +1,26 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Commands.UpdateProject
+{
+    public class UpdateProjectCommand : IRequest<ResultViewModel>
+    {
+        public int IdProject { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal TotalCost { get; set; }
+
+        public UpdateProjectCommand(int idProject, string title, string description, decimal totalCost)
+        {
+            IdProject = idProject;
+            Title = title;
+            Description = description;
+            TotalCost = totalCost;
+        }
+    }
+}
