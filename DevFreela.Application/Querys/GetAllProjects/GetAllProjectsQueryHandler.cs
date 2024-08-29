d7395010c991bdc9d10d3e6f3b3041f1c6563109
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-28 21:13:34 -0300

    @GustavoCarvalho25 - Adicionando Querys e passando projeto para CQRS

---
+++ DevFreela.Application/Querys/GetAllProjects/GetAllProjectsQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Application.Querys.GetAllProjects
+{
+    public class GetAllProjectsQueryHandler : IRequestHandler<GetAllProjectsQuery, ResultViewModel<List<ProjectItemViewModel>>>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public GetAllProjectsQueryHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel<List<ProjectItemViewModel>>> Handle(
+            GetAllProjectsQuery request,
+            CancellationToken cancellationToken)
+        {
+            var projects = await _context.Projects
+                .Include(p => p.Client)
+                .Include(p => p.Freelancer)
+                .Where(p => !p.IsDeleted && (request.Search == "" || p.Title.Contains(request.Search) || p.Description.Contains(request.Search)))
+                .Skip(request.Page * request.Size)
+                .Take(request.Size)
+                .ToListAsync();
+
+            var projectsViewModel = projects
+                .Select(p => ProjectItemViewModel.FromEntity(p))
+                .ToList();
+
+            return ResultViewModel<List<ProjectItemViewModel>>.Success(projectsViewModel);
+        }
+    }
+}
