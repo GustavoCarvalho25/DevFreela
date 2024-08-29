5e0f4f3996c1b359d93e8ffa99973568c4f70a55
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-28 21:15:04 -0300

    @GustavoCarvalho25 - Adicionando Command Insert Project e passando projeto para CQRS

---
+++ DevFreela.Application/Commands/InsertProject/InsertProjectHandler.cs
@@ -0,0 +1,32 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Application.Notification;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+
+namespace DevFreela.Application.Commands.InsertProject
+{
+    public class InsertProjectHandler : IRequestHandler<InsertProjectCommand, ResultViewModel<int>>
+    {
+        private readonly DevFreelaDbContext _context;
+        private readonly IMediator _mediator;
+
+        public InsertProjectHandler(DevFreelaDbContext context, IMediator mediator)
+        {
+            _context = context;
+            _mediator = mediator;
+        }
+
+        public async Task<ResultViewModel<int>> Handle(InsertProjectCommand request, CancellationToken cancellationToken)
+        {
+            var project = request.ToEntity();
+
+            await _context.Projects.AddAsync(project);
+            await _context.SaveChangesAsync();
+
+            var projectCreated = new ProjectCreatedNotification(project.Id, project.Title, project.TotalCost);
+            await _mediator.Publish(projectCreated);
+
+            return ResultViewModel<int>.Success(project.Id);
+        }
+    }
+}
