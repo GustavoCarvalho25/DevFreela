a34b9ab53a726dcfcabba63cbf6266c070e04d99
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-28 21:15:47 -0300

    @GustavoCarvalho25 - Adicionando padrão Notification e passando projeto para CQRS

---
+++ DevFreela.Application/Notification/GenerateBoardProjectHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Notification
+{
+    public class GenerateBoardProjectHandler : INotificationHandler<ProjectCreatedNotification>
+    {
+        public Task Handle(ProjectCreatedNotification notification, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Board criado para projeto: {notification.Title}");
+                
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ DevFreela.Application/Notification/ProjectCreatedNotification.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace DevFreela.Application.Notification
+{
+    public class ProjectCreatedNotification : INotification
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public decimal TotalCost { get; private set; }
+
+        public ProjectCreatedNotification(int id, string title, decimal totalCost)
+        {
+            Id = id;
+            Title = title;
+            TotalCost = totalCost;
+        }
+    }
+}
