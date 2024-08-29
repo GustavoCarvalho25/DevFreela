c96296f2e8de2fe7be1ea417230efd905b6681a7
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-28 21:15:13 -0300

    @GustavoCarvalho25 - Adicionando Command Start e passando projeto para CQRS

---
+++ DevFreela.Application/Commands/StartProject/StartProjectCommand.cs
@@ -0,0 +1,15 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+
+namespace DevFreela.Application.Commands.StartProject
+{
+    public class StartProjectCommand : IRequest<ResultViewModel>
+    {
+        public int Id { get; set; }
+
+        public StartProjectCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
