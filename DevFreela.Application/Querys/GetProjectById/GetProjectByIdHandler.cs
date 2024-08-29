d76ad0a92835d2752b7cafb20da880def541a8bf
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-28 21:13:54 -0300

    @GustavoCarvalho25 - Adicionando Querys GetProjectById e passando projeto para CQRS

---
+++ DevFreela.Application/Querys/GetProjectById/GetProjectByIdHandler.cs
@@ -0,0 +1,35 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Application.Querys.GetProjectById
+{
+    public class GetProjectByIdHandler : IRequestHandler<GetProjectByIdQuery, ResultViewModel>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public GetProjectByIdHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel> Handle(GetProjectByIdQuery request, CancellationToken cancellationToken)
+        {
+            var project = await _context.Projects
+            .Include(p => p.Client)
+            .Include(p => p.Freelancer)
+            .Include(p => p.Comments)
+            .SingleOrDefaultAsync(p => p.Id == request.Id);
+
+            if (project is null)
+            {
+                return ResultViewModel<ProjectViewModel>.Error("Projeto não existe.");
+            }
+
+            var model = ProjectViewModel.FromEntity(project);
+
+            return ResultViewModel<ProjectViewModel>.Success(model);
+        }
+    }
+}
