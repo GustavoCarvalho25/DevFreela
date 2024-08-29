5432160f5d0cf8ea9d07972c4ece45ba749113e1
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-28 21:14:22 -0300

    @GustavoCarvalho25 - Adicionando Command Complete e passando projeto para CQRS

---
+++ DevFreela.Application/Commands/CompleteProject/CompleteProjectCommand.cs
@@ -0,0 +1,20 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Commands.CompleteProject
+{
+    public class CompleteProjectCommand : IRequest<ResultViewModel>
+    {
+        public int Id { get; set; }
+
+        public CompleteProjectCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ DevFreela.Application/Commands/CompleteProject/CompleteProjectHandler.cs
@@ -0,0 +1,34 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Application.Commands.CompleteProject
+{
+    public class CompleteProjectHandler : IRequestHandler<CompleteProjectCommand, ResultViewModel>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public CompleteProjectHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel> Handle(CompleteProjectCommand request, CancellationToken cancellationToken)
+        {
+            
+            var project = await _context.Projects.SingleOrDefaultAsync(p => p.Id == request.Id);
+
+            if (project is null)
+            {
+                return ResultViewModel.Error("Projeto não existe.");
+            }
+
+            project.Complete();
+            _context.Update(project);
+            _context.SaveChangesAsync();
+
+            return ResultViewModel.Success();
+        }
+    }
+}
