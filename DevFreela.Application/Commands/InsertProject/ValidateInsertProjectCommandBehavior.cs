123a1c419b8bf9b666a0977c9fc70552bfdeb18f
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-30 22:15:43 -0300

    @GustavoCarvalho25 - Aplicando padrão Repository em Insert project

--- DevFreela.Application/Commands/InsertProject/ValidateInsertProjectCommandBehavior.cs
+++ DevFreela.Application/Commands/InsertProject/ValidateInsertProjectCommandBehavior.cs
@@ -1,11 +1,6 @@
 ﻿using DevFreela.Application.Models;
 using DevFreela.Infrastructure.Persistence;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DevFreela.Application.Commands.InsertProject
 {
