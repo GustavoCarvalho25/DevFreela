9d665148dd4bfee8dd2ed61cacd142743135fda0
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-30 22:17:59 -0300

    @GustavoCarvalho25 - Implementando camada de Repository

---
+++ DevFreela.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,39 @@
+﻿using DevFreela.Core.Repositories;
+using DevFreela.Infrastructure.Persistence;
+using DevFreela.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure
+{
+    public static class InfrastructureModule
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddRepositories()
+                .AddData(configuration);
+            return services;
+        }
+
+        private static IServiceCollection AddData(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DevFreelaCs");
+            services.AddDbContext<DevFreelaDbContext>(o => o.UseSqlServer(connectionString));
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IProjectRepository, ProjectRepository>();
+
+            return services;
+        }
+    }
+}
