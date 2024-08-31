ce312114f9bd77a1172fa7f1c6cc942874044d4a
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-30 22:16:57 -0300

    @GustavoCarvalho25 - Aplicando padrão Repository em Querys GetProjectById

--- DevFreela.Application/Querys/GetProjectById/GetProjectByIdHandler.cs
+++ DevFreela.Application/Querys/GetProjectById/GetProjectByIdHandler.cs
@@ -1,26 +1,21 @@
 ﻿using DevFreela.Application.Models;
-using DevFreela.Infrastructure.Persistence;
+using DevFreela.Core.Repositories;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.Application.Querys.GetProjectById
 {
     public class GetProjectByIdHandler : IRequestHandler<GetProjectByIdQuery, ResultViewModel>
     {
-        private readonly DevFreelaDbContext _context;
+        private readonly IProjectRepository _projectRepository;
 
-        public GetProjectByIdHandler(DevFreelaDbContext context)
+        public GetProjectByIdHandler(IProjectRepository projectRepository)
         {
-            _context = context;
+            _projectRepository = projectRepository;
         }
 
         public async Task<ResultViewModel> Handle(GetProjectByIdQuery request, CancellationToken cancellationToken)
         {
-            var project = await _context.Projects
-            .Include(p => p.Client)
-            .Include(p => p.Freelancer)
-            .Include(p => p.Comments)
-            .SingleOrDefaultAsync(p => p.Id == request.Id);
+            var project = await _projectRepository.GetDetailsById(request.Id);
 
             if (project is null)
             {
