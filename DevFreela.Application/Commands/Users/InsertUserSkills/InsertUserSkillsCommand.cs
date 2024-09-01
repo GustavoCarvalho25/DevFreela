9f8bfcde613887df453fdfb6dc090cc9b7d5062d
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-09-01 20:46:43 -0300

    @GustavoCarvalho25 - Criando Insert User Skills CQRS

--- DevFreela.Application/Commands/Users/InsertUserSkills/InsertUserSkillsCommand.cs
+++ DevFreela.Application/Commands/Users/InsertUserSkills/InsertUserSkillsCommand.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DevFreela.Application.Models;
+using MediatR;
 
 namespace DevFreela.Application.Commands.Users.InsertUserSkills
 {
-    internal class InsertUserSkillsCommand
+    public class InsertUserSkillsCommand : IRequest<ResultViewModel<int>>
     {
+        public int[] SkillIds { get; set; }
+        public int IdUser { get; set; }
+
+        public InsertUserSkillsCommand(int[] skillIds, int idUser)
+        {
+            SkillIds = skillIds;
+            IdUser = idUser;
+        }
     }
 }
--- DevFreela.Application/Commands/Users/InsertUserSkills/InsertUserSkillsHandler.cs
+++ DevFreela.Application/Commands/Users/InsertUserSkills/InsertUserSkillsHandler.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using MediatR;
 
 namespace DevFreela.Application.Commands.Users.InsertUserSkills
 {
-    public class InsertUserSkillsHandler
+    public class InsertUserSkillsHandler : IRequestHandler<InsertUserSkillsCommand, ResultViewModel<int>>
     {
+        private readonly IUserRepository _repository;
+
+        public InsertUserSkillsHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ResultViewModel<int>> Handle(InsertUserSkillsCommand request, CancellationToken cancellationToken)
+        {
+            var exists = await _repository.CheckUserExists(request.IdUser);
+
+            if (!exists)
+            {
+                return ResultViewModel<int>.Error("Usuário não existe.");
+            }
+
+            var userSkills = request.SkillIds.Select(s =>
+            new UserSkill(request.IdUser, s)).ToList();
+
+            await _repository.AddUserSkills(userSkills);
+
+            return ResultViewModel<int>.Success(request.IdUser);
+        }
     }
 }
