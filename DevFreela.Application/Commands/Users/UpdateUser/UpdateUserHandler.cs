830105a0d251ef6ffb99e658397383b6ea6e6195
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-09-01 20:47:06 -0300

    @GustavoCarvalho25 - Implementando Update User CQRS

---
+++ DevFreela.Application/Commands/Users/UpdateUser/UpdateUserHandler.cs
@@ -0,0 +1,31 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Repositories;
+using MediatR;
+
+namespace DevFreela.Application.Commands.Users.UpdateUser
+{
+    public class UpdateUserHandler : IRequestHandler<UpdateUserCommand, ResultViewModel>
+    {
+        private readonly IUserRepository _repository;
+
+        public UpdateUserHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ResultViewModel> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _repository.GetById(request.Id);
+
+            if (user is null)
+            {
+                return ResultViewModel.Error("Usuário não existe.");
+            }
+
+            user.Update(request.FullName, request.Email, request.BirthDate);
+            await _repository.Update(user);
+
+            return ResultViewModel.Success();
+        }
+    }
+}
