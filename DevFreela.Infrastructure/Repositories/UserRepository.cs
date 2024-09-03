f0777f4d10c0496176c453e4de6434343a978772
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-09-02 23:36:12 -0300

    @GustavoCarvalho25 - Implementando Repositório de Usuário

--- DevFreela.Infrastructure/Repositories/UserRepository.cs
+++ DevFreela.Infrastructure/Repositories/UserRepository.cs
@@ -1,36 +1,51 @@
 ﻿using DevFreela.Core.Entities;
 using DevFreela.Core.Repositories;
+using DevFreela.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.Infrastructure.Repositories
 {
     public class UserRepository : IUserRepository
     {
-        public Task<int> Add(User user)
+        private readonly DevFreelaDbContext _context;
+
+        public UserRepository(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Add(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return await Task.FromResult(user.Id);
+        }
+
+        public async Task<bool> CheckUserExists(int id)
+        {
+            var userExist = await _context.Users.AnyAsync(u => u.Id == id);
+
+            return userExist;
+
+        }
+
+        public async Task<User?> GetById(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<User?> GetDetailsById(int id)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> CheckUserExists(int id)
+        public async Task AddUserSkills(List<UserSkill> skills)
         {
             throw new NotImplementedException();
         }
 
-        public Task<User?> GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<User?> GetDetailsById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task AddUserSkills(List<UserSkill> skills)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Update(User user)
+        public async Task Update(User user)
         {
             throw new NotImplementedException();
         }
