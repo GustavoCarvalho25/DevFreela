b3c7ba831ba1dd8f00cf97081342efeca0ec614d
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-09-02 23:36:29 -0300

    @GustavoCarvalho25 - Implementando GetUser CQRS Querys

---
+++ DevFreela.Application/Querys/Users/GetUserById/GetUserByIdHandler.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Repositories;
+using MediatR;
+
+namespace DevFreela.Application.Querys.Users.GetUserById
+{
+    public class GetUserByIdHandler : IRequestHandler<GetUserByIdQuery, ResultViewModel>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUserByIdHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<ResultViewModel> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetById(request.Id);
+
+            if (user is null)
+            {
+                return ResultViewModel.Error("Usuário não existe.");
+            }
+
+            var model = UserViewModel.FromEntity(user);
+
+            return ResultViewModel<UserViewModel>.Success(model);
+        }
+    }
+}
