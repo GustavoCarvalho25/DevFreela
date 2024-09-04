54932838fc25b23424732e75f0c9aad1f1cc6883
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-09-04 20:59:07 -0300

    @GustavoCarvalho25 - Implementando CQRS de Update Skills

---
+++ DevFreela.Application/Commands/Skills/UpdateSkill/UpdateSkillHandler.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Repositories;
+using MediatR;
+
+namespace DevFreela.Application.Commands.Skills.UpdateSkill
+{
+    public class UpdateSkillHandler : IRequestHandler<UpdateSkillCommand, ResultViewModel>
+    {
+        private readonly ISkillRepository _skillRepository;
+
+        public UpdateSkillHandler(ISkillRepository skillRepository)
+        {
+            _skillRepository = skillRepository;
+        }
+
+        public async Task<ResultViewModel> Handle(UpdateSkillCommand request, CancellationToken cancellationToken)
+        {
+            var skill = await _skillRepository.GetById(request.Id);
+
+            if (skill == null)
+            {
+                return ResultViewModel.Error("Skill não existe.");
+            }
+
+            await _skillRepository.Update(skill);
+
+            return ResultViewModel.Success();
+        }
+    }
+}
