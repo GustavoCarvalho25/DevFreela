9dcf344a08c0ce5272b51eac96f6838f105711e7
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-09-04 21:01:20 -0300

    @GustavoCarvalho25 - Implementando CQRS de GetSkills por Id

---
+++ DevFreela.Application/Querys/Skills/GetSkillById/GetSkillByIdHandler.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Repositories;
+using MediatR;
+
+namespace DevFreela.Application.Querys.Skills.GetSkillById
+{
+    public class GetSkillByIdHandler : IRequestHandler<GetSkillByIdQuery, ResultViewModel>
+    {
+        private readonly ISkillRepository _skillRepository;
+
+        public GetSkillByIdHandler(ISkillRepository skillRepository)
+        {
+            _skillRepository = skillRepository;
+        }
+
+        public async Task<ResultViewModel> Handle(GetSkillByIdQuery request, CancellationToken cancellationToken)
+        {
+            var skill = await _skillRepository.GetById(id: request.Id);
+
+            if (skill == null)
+            {
+                return ResultViewModel.Error("Skill não existe.");
+            }
+
+            var model = SkillViewModel.ConvertToViewModel(skill);
+
+            return ResultViewModel<SkillViewModel>.Success(model);
+        }
+    }
+}
---
+++ DevFreela.Application/Querys/Skills/GetSkillById/GetSkillByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+
+namespace DevFreela.Application.Querys.Skills.GetSkillById
+{
+    public class GetSkillByIdQuery : IRequest<ResultViewModel>
+    {
+        public int Id { get; set; }
+
+        public GetSkillByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
