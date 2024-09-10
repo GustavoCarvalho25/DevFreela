b346dbd279306bccc9fbb74a96cad46934facec4
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-09-10 18:21:53 -0300

    @GustavoCarvalho25 - Adicionando Atributos de Login nos Commands de Usuario

--- DevFreela.Application/Commands/Users/InsertUser/InsertUserHandler.cs
+++ DevFreela.Application/Commands/Users/InsertUser/InsertUserHandler.cs
@@ -1,5 +1,6 @@
 ﻿using DevFreela.Application.Models;
 using DevFreela.Core.Repositories;
+using DevFreela.Core.Services;
 using MediatR;
 
 namespace DevFreela.Application.Commands.Users.InsertUser
@@ -7,19 +8,29 @@ namespace DevFreela.Application.Commands.Users.InsertUser
     public class InsertUserHandler : IRequestHandler<InsertUserCommand, ResultViewModel<int>>
     {
         private readonly IUserRepository _userRepository;
+        private readonly IAuthService _authService;
 
-        public InsertUserHandler(IUserRepository repository)
+        public InsertUserHandler(IUserRepository repository, IAuthService authService)
         {
             _userRepository = repository;
+            _authService = authService;
         }
 
         public async Task<ResultViewModel<int>> Handle(InsertUserCommand request, CancellationToken cancellationToken)
         {
+            HashPassword(request);
+
             var user = request.ToEntity();
 
             await _userRepository.Add(user);
 
             return ResultViewModel<int>.Success(user.Id);
+        }
+
+        private void HashPassword(InsertUserCommand request)
+        {
+            var passwordHash = _authService.ComputeSha256Hash(request.Password);
+            request.SetHashingInPassord(passwordHash);
         }
     }
 }
