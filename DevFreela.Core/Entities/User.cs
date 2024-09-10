c873b64b6bc22f6c551a97f21f952e5139f33848
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-09-10 18:22:11 -0300

    @GustavoCarvalho25 - Adicionando Atributos de Login nos Usuarios

--- DevFreela.Core/Entities/User.cs
+++ DevFreela.Core/Entities/User.cs
@@ -6,12 +6,14 @@
         public string Email { get; private set; }
         public DateTime BirthDate { get; private set; }
         public bool Active { get; private set; }
+        public string Password { get; private set; }
+        public string Role { get; private set; }
         public List<UserSkill> Skills { get; private set; }
         public List<Project> OwnedProjects { get; private set; }
         public List<Project> FreelanceProjects { get; private set; }
         public List<ProjectComment> Comments { get; private set; }
 
-        public User(string fullName, string email, DateTime birthDate) : base()
+        public User(string fullName, string email, DateTime birthDate, string password, string role) : base()
         {
             FullName = fullName;
             Email = email;
@@ -22,6 +24,8 @@
             OwnedProjects = [];
             FreelanceProjects = [];
             Comments = [];
+            Password = password;
+            Role = role;
         }
 
         public void Update(string fullName, string email, DateTime birthDate)
