ebde031fe30182808b38861487d0db49c73f569f
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-09-15 22:55:05 -0300

    @GustavoCarvalho25 - Adicionando Autenticação e Autorização com JWT

--- DevFreela.Infrastructure/InfrastructureModule.cs
+++ DevFreela.Infrastructure/InfrastructureModule.cs
@@ -3,9 +3,12 @@ using DevFreela.Core.Services;
 using DevFreela.Infrastructure.Persistence;
 using DevFreela.Infrastructure.Repositories;
 using DevFreela.Infrastructure.Services.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace DevFreela.Infrastructure
 {
@@ -14,16 +17,32 @@ namespace DevFreela.Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services
+                .AddSwaggerAutentication(configuration)
                 .AddRepositories()
                 .AddData(configuration)
                 .AddServices(configuration);
             return services;
         }
-
-        private static IServiceCollection AddData(this IServiceCollection services, IConfiguration configuration)
+        private static IServiceCollection AddSwaggerAutentication(this IServiceCollection services, IConfiguration configuration)
         {
-            var connectionString = configuration.GetConnectionString("DevFreelaCs");
-            services.AddDbContext<DevFreelaDbContext>(o => o.UseSqlServer(connectionString));
+            services
+              .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options =>
+              {
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      ValidateIssuer = true,
+                      ValidateAudience = true,
+                      ValidateLifetime = true,
+                      ValidateIssuerSigningKey = true,
+
+                      ValidIssuer = configuration["Jwt:Issuer"],
+                      ValidAudience = configuration["Jwt:Audience"],
+                      IssuerSigningKey = new SymmetricSecurityKey
+                    (Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+                  };
+              });
+
             return services;
         }
 
@@ -36,10 +55,18 @@ namespace DevFreela.Infrastructure
             return services;
         }
 
+        private static IServiceCollection AddData(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DevFreelaCs");
+            services.AddDbContext<DevFreelaDbContext>(o => o.UseSqlServer(connectionString));
+            return services;
+        }
+
         private static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddScoped<IAuthService, AuthService>();
             return services;
         }
+
     }
 }
