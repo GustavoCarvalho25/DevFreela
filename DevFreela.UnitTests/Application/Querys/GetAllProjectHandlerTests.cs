ec58aae10e8215312a3055158900c43dab3d9a0b
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-09-18 11:58:28 -0300

    @GustavoCarvalho25 - Corrigindo Teste de Handler GetAll

--- DevFreela.UnitTests/Application/Querys/GetAllProjectHandlerTests.cs
+++ DevFreela.UnitTests/Application/Querys/GetAllProjectHandlerTests.cs
@@ -13,9 +13,9 @@ namespace DevFreela.UnitTests.Application.Querys
             // Arrange
             var projects = new List<Project>
             {
-                new Project("Nome do Teste 1", "Descricao De Teste 1", 2, 1, 10000),
-                new Project("Nome do Teste 2", "Descricao De Teste 2", 2, 1, 20000),
-                new Project("Nome do Teste 3", "Descricao De Teste 3", 2, 1, 30000)
+                CreateProjectWithClientAndFreelancer(new Project("Nome do Teste 1", "Descricao De Teste 1", 2, 1, 10000)),
+                CreateProjectWithClientAndFreelancer(new Project("Nome do Teste 2", "Descricao De Teste 2", 2, 1, 20000)),
+                CreateProjectWithClientAndFreelancer(new Project("Nome do Teste 3", "Descricao De Teste 3", 2, 1, 30000))
             };
 
             var projectRepositoryMock = new Mock<IProjectRepository>();
@@ -38,6 +38,18 @@ namespace DevFreela.UnitTests.Application.Querys
                 getAllProjectsQuery.Page,
                 getAllProjectsQuery.Size)
             .Result, Times.Once);
+        }
+
+        private Project CreateProjectWithClientAndFreelancer(Project project)
+        {
+            var client = new User("Gustavo Teste", "gustavo@email.com", DateTime.Now.AddYears(-20), "SenhaTeste", "Client");
+            var freelancer = new User("Gabriel Teste", "gabriel@email.com", DateTime.Now.AddYears(-20), "SenhaTeste", "Freelancer");
+
+            // Usando reflexão para definir as propriedades privadas
+            typeof(Project).GetProperty("Client").SetValue(project, client);
+            typeof(Project).GetProperty("Freelancer").SetValue(project, freelancer);
+
+            return project;
         }
     }
 }
