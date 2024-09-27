f97ee1868aba0d1a3e19ab4bbefa079105a99f1c
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-09-27 12:47:18 -0300

    @GustavoCarvalho25 - Implementando DTO e IPaymentService para sincronização com microsserviço

---
+++ DevFreela.Core/DTOs/PaymentInfoDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace DevFreela.Core.DTOs
+{
+    public class PaymentInfoDTO
+    {
+        public int IdProject { get; private set; }
+        public string CreditCardNumber { get; private set; }
+        public string Cvv { get; private set; }
+        public string ExpiresAt { get; private set; }
+        public string FullName { get; private set; }
+        public decimal Amount { get; private set; }
+
+        public PaymentInfoDTO(int idProject, string creditCardNumber, string cvv, string expiresAt, string fullName, decimal amount)
+        {
+            IdProject = idProject;
+            CreditCardNumber = creditCardNumber;
+            Cvv = cvv;
+            ExpiresAt = expiresAt;
+            FullName = fullName;
+            Amount = amount;
+        }
+    }
+}
---
+++ DevFreela.Core/Services/IPaymentService.cs
@@ -0,0 +1,9 @@
+﻿using DevFreela.Core.DTOs;
+
+namespace DevFreela.Core.Services
+{
+    public interface IPaymentService
+    {
+        public Task<bool> ProcessPayment(PaymentInfoDTO paymentInfoDTO);
+    }
+}
