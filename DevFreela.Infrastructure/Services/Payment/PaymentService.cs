36fca04be79ca7b460196ab2727cfae3fdc04148
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-09-27 12:48:12 -0300

    @GustavoCarvalho25 - Implementando comunicação com serviço de pagamentos

---
+++ DevFreela.Infrastructure/Services/Payment/PaymentService.cs
@@ -0,0 +1,35 @@
+﻿using Azure;
+using DevFreela.Core.DTOs;
+using DevFreela.Core.Services;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.Text.Json;
+
+namespace DevFreela.Infrastructure.Services.Payment
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly string _paymentsBaseUrl;
+
+        public PaymentService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _paymentsBaseUrl = configuration.GetSection("Services:Payments").Value;
+        }
+
+        public async Task<bool> ProcessPayment(PaymentInfoDTO paymentInfoDTO)
+        {
+            var url = $"{_paymentsBaseUrl}/api/payments";
+            var pageInfoJson = JsonSerializer.Serialize(paymentInfoDTO);
+
+            var paymentContent = new StringContent(pageInfoJson, Encoding.UTF8, "application/json");
+
+            var httpClient = _httpClientFactory.CreateClient("Payments");
+
+            var response = await httpClient.PostAsync(url, paymentContent);
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
